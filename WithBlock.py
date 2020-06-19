469c51bebe5adb9b04f9124bbe887178a737ba5b
MANOJ M JADHAV <jadhavmanoj369@gmail.com>
2020-06-19 13:48:53 +0530

    first commit

---
+++ WithBlock.py
@@ -0,0 +1,8 @@
+with  open("data.txt","a" ) as f:
+    f.write("yoyo honey singh\n")  #here we are appending the new data to the file
+
+
+
+# f=open("data.txt" ,"r")
+# content=f.read()
+# print(content)
